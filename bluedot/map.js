e4016eb18a3750db69dee06120f64ad18c8e1257
George Verghese <109232709+gv2325@users.noreply.github.com>
2022-11-30 12:51:28 -0500

    map.js is updated with gmaps api

--- bluedot/map.js
+++ bluedot/map.js
@@ -1,22 +1,52 @@
-
-let map;
-
 function initMap() {
-    const originalMapCenter = new google.maps.LatLng(40.77, -73.77);
+    const myLatlng = { lat: 40.806912, lng: -73.963060 };
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 0,
-        center: originalMapCenter,
+        zoom: 3,
+        center: myLatlng,
+        disableDefaultUI: true,
+        zoomControl: true,
+    });
+    const marker = new google.maps.Marker({
+        position: myLatlng,
+        map,
+        title: "Click to zoom",
     });
 
-    const infowindow = new google.maps.InfoWindow({
-        content: "Change the zoom level",
-        position: originalMapCenter,
+    map.addListener("center_changed", () => {
+      // 3 seconds after the center of the map has changed, pan back to the
+      // marker.
+        window.setTimeout(() => {
+        map.panTo(marker.getPosition());
+        }, 3000);
+    });
+    marker.addListener("click", () => {
+        map.setZoom(19);
+        map.setCenter(marker.getPosition());
     });
 
-    infowindow.open(map);
-    map.addListener("zoom_changed", () => {
-        infowindow.setContent("Zoom: " + map.getZoom());
+    google.maps.event.addListener(map, 'click', function (event) {
+        displayCoordinates(event.latLng);               
     });
+
+            // Create the initial InfoWindow.
+        let infoWindow = new google.maps.InfoWindow({
+            content: "Click the map to get Lat/Lng!",
+            position: myLatlng,
+        });
+        infoWindow.open(map);
+        // Configure the click listener.
+        map.addListener("click", (mapsMouseEvent) => {
+            // Close the current InfoWindow.
+            infoWindow.close();
+            // Create a new InfoWindow.
+            infoWindow = new google.maps.InfoWindow({
+                position: mapsMouseEvent.latLng,
+            });
+            infoWindow.setContent(
+                JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+            );
+            infoWindow.open(map);
+        });
 }
 
 window.initMap = initMap;
